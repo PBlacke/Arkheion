bd9a86ce59bcb8e476fa1f2972eaf6922a3260b6
TechnoX0 <meroypaulvincent@gmail.com>
2025-09-16 21:09:38 +0800

    styling changes

--- includes/footer.php
+++ includes/footer.php
@@ -1,31 +1,43 @@
-<section class="footer">
-    <div class="box-container">
-        <div class="box">
-            <h3 style="color: #25ae60; font-size: 22px;">WE'RE LOCATED AT</h3>
-            <a href="#" style="cursor:text;"><i class="fas fa-location-dot"></i> A. Bonifacio St., <br><br>Ormoc City, Leyte 6541 </a>
+<footer class="footer sm:footer-horizontal bg-base-100 text-base-content p-10">
+    <aside>
+        <a href="./index.php" class="navbar-start h-full">
+            <img src="image/LOGO.png" class="h-full">
+        </a>
+        <p>
+            A Comprehensive Research Repository
+            <br>
+            with Intelligent Analytics
+        </p>
+    </aside>
+    <nav>
+        <h6 class="footer-title">We're Located At</h6>
+        <a href="#" class="link link-hover">A. Bonifacio St. <br> Ormoc City, Leyte 6541</a>
+    </nav>
+    <nav>
+        <h6 class="footer-title">Extras</h6>
+        <a href="../terms.php" class="link link-hover">Privacy Policy</a>
+        <a href="../terms.php" class="link link-hover">Terms & Conditions</a>
+        <a href="../archive.php" class="link link-hover">Archive</a>
+    </nav>
+    <nav>
+        <h6 class="footer-title">Contact Us</h6>
+        <a href="#" class="link link-hover cursor-text">+63 9*** ** ****</a>
+        <a href="#" class="link link-hover">arkheion@sample.com</a>
+    </nav>
+    <nav class="share">
+        <h6 class="footer-title">Follow Us</h6>
+        <div class="grid grid-flow-col gap-3">
+            <a href="https://www.facebook.com/" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+            <a href="https://www.youtube.com/" target="_blank"><i class="fa-brands fa-youtube"></i></a>
+            <a href="https://www.google.com/" target="_blank"><i class="fa-brands fa-google"></i></i></a>
+            <a href="https://www.twitter.com/" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
         </div>
+    </nav>
 
-        <div class="box">
-            <h3 style="color: #25ae60; font-size: 22px;">Extras</h3>
-            <a href="terms.php" target="_blank"><i class="fas fa-arrow-right"> </i> Privacy Policy</a>
-            <a href="terms.php" target="_blank"><i class="fas fa-arrow-right"></i> Terms & Conditions</a>
-            <a href="archive.php" target="_blank"><i class="fas fa-arrow-right"></i> Archive</a>
-        </div>
-
-        <div class="box">
-            <h3 style="color: #25ae60; font-size: 22px;">Contact Us</h3>
-            <a href="#" style="cursor:text;"><i class="fas fa-phone" style="color:#25ae60; font-size: 15px;"></i> +63 9*** ** ****</a>
-            <a href="#" target="_blank"><i class="fas fa-envelope"></i>arkheion@sample.com</a>
-        </div>
-    </div>
-
-    <div class="share">
-        <h3>Follow Us</h3>
-        <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a>
-        <a href="https://www.youtube.com/" target="_blank"><i class="fab fa-youtube"></i></a>
-        <a href="https://www.google.com/" target="_blank"><i class="fab fa-google-plus"></i></a>
-        <a href="https://www.twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
-    </div>
-
-    <div class="credit">Created by <span>Donaire & Ubay</span> | All rights reserved.</div>
-</section>
+    <!-- <div class="credit">Created by <span>Donaire & Ubay</span> | All rights reserved.</div> -->
+</footer>
+<footer class="footer sm:footer-horizontal footer-center bg-base-100 text-base-content p-4">
+    <aside>
+        <p>Copyright © <?php echo date("Y"); ?> - All right reserved by Donaire & Ubay</p>
+    </aside>
+</footer>
--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,4 @@
-<header class="navbar bg-base-100 shadow-sm h-20">
+<header class="navbar bg-base-100 shadow-sm h-14">
     <a href="./index.php" class="navbar-start h-full">
         <img src="image/LOGO.png" class="h-full">
     </a>
@@ -7,7 +7,6 @@
         <button type="submit" class="fas fa-search" aria-label="Search"></button>
     </form>
     <div class="navbar-end">
-        <div id="search-btn" style="color: #0c1776;" class="fas fa-search"></div>
-        <div id="login-btn" style="color: #0c1776;"><a href='./auth/login.php'>Login</a></div>
+        <a href='./auth/login.php' class="btn btn-primary rounded-full">SIGN IN</a>
     </div>
 </header>
