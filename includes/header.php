8d240e5a5c27aded55aaf1553cc7b85f9724abf2
TechnoX0 <meroypaulvincent@gmail.com>
2025-09-22 21:12:21 +0800

    added log out button to header if already logged in

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,17 @@
+<?php
+require './config/connection.php';
+require_once './config/auth.php';
+require_once 'database.php';
+
+session_start();
+
+$loggedInUser = getLoggedInUser($db);
+
+if (isset($_POST['dashboardButton']) && $loggedInUser) {
+    redirectToDashboard($loggedInUser['role']);
+}
+?>
+
 <header class="navbar bg-base-100 shadow-sm h-14">
     <a href="./index.php" class="navbar-start h-full">
         <img src="image/LOGO.png" class="h-full">
@@ -6,7 +20,16 @@
         <input type="search" name="searchbar" placeholder="Search by title..." id="search-box">
         <button type="submit" class="fas fa-search" aria-label="Search"></button>
     </form>
-    <div class="navbar-end">
-        <a href='./auth/login.php' class="btn btn-primary rounded-full">SIGN IN</a>
-    </div>
+    <?php if ($loggedInUser): ?>
+        <div class="navbar-end gap-2">
+            <form method="post">
+                <button type="submit" name="dashboardButton" class="btn btn-primary">Dashbaord</button>
+            </form>
+            <a href="./auth/logout.php" class="btn btn-primary">Logout</a>
+        </div>
+    <?php else: ?>
+        <div class="navbar-end">
+            <a href='./auth/login.php' class="btn btn-primary rounded-full">Login</a>
+        </div>
+    <?php endif; ?>
 </header>
